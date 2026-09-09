5eb9502b0675e1384aecf1a4d1509eb20c8826c1
László <laszlo.2099@gmail.com>
2026-09-08 20:23:44 -0600

    Firts commit.

---
+++ src/main/java/mx/unadmexico/rupe/RupeApplication.java
@@ -0,0 +1,13 @@
+package mx.unadmexico.rupe;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupeApplication {
+
+  public static void main(String[] args) {
+    run(RupeApplication.class, args);
+  }
+}
---
+++ src/test/java/mx/unadmexico/rupe/RupeApplicationTests.java
@@ -0,0 +1,11 @@
+package mx.unadmexico.rupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
