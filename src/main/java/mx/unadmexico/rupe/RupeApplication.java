609ba4cdf40eb5beac7c2c6e251b809f8eda70ea
László <laszlo.2099@gmail.com>
2026-09-10 18:15:15 -0600

    Lombok version & banner updated.

--- src/main/java/mx/unadmexico/rupe/RupeApplication.java
+++ src/main/java/mx/unadmexico/rupe/RupeApplication.java
@@ -2,12 +2,15 @@ package mx.unadmexico.rupe;
 
 import static org.springframework.boot.SpringApplication.run;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class RupeApplication {
 
   public static void main(String[] args) {
     run(RupeApplication.class, args);
+    log.info("\n\033[0;92mRUPE started!\u001B[0m");
   }
 }
